5d6b6a32c471f862b2142ccf17ee06b7f2be601a
kristiyandimitrov93-cpu <kristiyan.dimitrov93@gmail.com>
2026-01-29 15:07:39 +0200

    adding routes for the rest of the links

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,14 @@ export const App = () => {
       <Route element={<AppLayout />}>
         <Route path='/' element={<Dashboard />} />
         <Route path='/cases' element={<PlaceholderPage title={'Cases'} />} />
+        <Route path='/legal-search' element={<PlaceholderPage title={'Legal Search'} />} />
+        <Route path='/smart-review' element={<PlaceholderPage title={'Smart Review'} />} />
+        <Route path='/compliance-view' element={<PlaceholderPage title={'Compliance View'} />} />
+        <Route path='/legal-forms' element={<PlaceholderPage title={'Legal Forms'} />} />
+        <Route path='/team' element={<PlaceholderPage title={'Team'} />} />
+        <Route path='/integrations' element={<PlaceholderPage title={'Integrations'} />} />
+        <Route path='/settings' element={<PlaceholderPage title={'Settings'} />} />
+        <Route path='/support' element={<PlaceholderPage title={'Support Center'} />} />
       </Route>
 
     </Routes>
