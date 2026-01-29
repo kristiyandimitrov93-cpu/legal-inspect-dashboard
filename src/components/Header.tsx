8e9f761ecb8aa86865a711d9ad70ec59cf63a1ca
kristiyandimitrov93-cpu <kristiyan.dimitrov93@gmail.com>
2026-01-29 22:44:12 +0200

    adding app header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,29 @@
+import { Bell, CircleUser, Plus, Search, Settings } from "lucide-react"
+import './Header.scss'
+
+
 export const Header = () => {
-    return (<div>Header</div>)
+    return (<header className="app-header">
+        <div className="left">
+            <h1 className="header-title">AI Analysis Overview</h1>
+        </div>
+        <div className="right">
+            <div className="search-input">
+                <Search className="search-icon" />
+                <input type="text" placeholder="Search..." />
+            </div>
+
+            <Plus className="header-icon" size={18} />
+
+            <Settings className="header-icon" size={18} />
+
+            <Bell className="header-icon" size={18} />
+
+            <CircleUser className="header-icon" size={18} />
+
+
+
+
+        </div>
+    </header>)
 }
