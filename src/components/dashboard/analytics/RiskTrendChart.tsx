615f6869be058ab0ea2082530463c734b4073e3c
kristiyandimitrov93-cpu <kristiyan.dimitrov93@gmail.com>
2026-02-01 14:45:22 +0200

    using card component for RiskTrendChart

--- src/components/dashboard/analytics/RiskTrendChart.tsx
+++ src/components/dashboard/analytics/RiskTrendChart.tsx
@@ -3,6 +3,7 @@ import { RechartsDevtools } from '@recharts/devtools';
 import { useDispatch, useSelector } from 'react-redux';
 import { chartDataSelector, showDocumentsLineSelectpr, showRiskLineSelector, toggleDocumentsLine, toggleRisksLine } from '../../../state/analytics';
 import './RiskTrendChart.scss'
+import { Card, CardBody, CardHeader, CardTitle } from '../../card/Card';
 
 
 export const RiskTrendChart = () => {
@@ -13,9 +14,8 @@ export const RiskTrendChart = () => {
     const showRisksLine = useSelector(showRiskLineSelector)
 
     return (
-        <div className='chart-card'>
-            <div className="chart-card-header">
-                <h3> AI Risk Trend</h3>
+        <Card className='risk-trend-chart-card'>
+            <CardHeader actionBtn={
                 <div className="chart-card-filters">
                     <button
                         className={`filter documents-filter ${showDocumentsLine ? 'active' : ''}`}
@@ -30,45 +30,52 @@ export const RiskTrendChart = () => {
                         With risks
                     </button>
                 </div>
-            </div>
-            <div className='chart-card-container'>
+
+            }>
+                <CardTitle>
+                    AI Risk Trend
+                </CardTitle>
+            </CardHeader>
+            <CardBody>
+                <div className='chart-card-container'>
 
 
-                <LineChart
-                    style={{ width: '100%', maxWidth: '700px', height: '100%', maxHeight: '70vh', aspectRatio: 1.618 }}
-                    responsive
-                    data={chartDate}
-                    margin={{
-                        top: 5,
-                        right: 0,
-                        left: 0,
-                        bottom: 5,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="month" />
-                    <YAxis width="auto" />
-                    <Tooltip />
-                    <Legend />
-                    {showDocumentsLine && (
-                        <Line type="monotone"
-                            dataKey="documentsAnalyzed"
-                            stroke="#467D95"
+                    <LineChart
+                        style={{ width: '100%', maxWidth: '700px', height: '100%', maxHeight: '70vh', aspectRatio: 1.618 }}
+                        responsive
+                        data={chartDate}
+                        margin={{
+                            top: 5,
+                            right: 0,
+                            left: 0,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="month" />
+                        <YAxis width="auto" />
+                        <Tooltip />
+                        <Legend />
+                        {showDocumentsLine && (
+                            <Line type="monotone"
+                                dataKey="documentsAnalyzed"
+                                stroke="#467D95"
+                                strokeWidth={2}
+                                dot={{ fill: '#467D95', strokeWidth: 2, r: 4 }}
+                                activeDot={{ r: 6 }}
+                                name="Documents analyzed" />)
+                        }
+                        {showRisksLine && (<Line type="monotone" dataKey="riskPct" stroke="#B87273"
                             strokeWidth={2}
-                            dot={{ fill: '#467D95', strokeWidth: 2, r: 4 }}
+                            dot={{ fill: '#B87273', strokeWidth: 2, r: 4 }}
                             activeDot={{ r: 6 }}
-                            name="Documents analyzed" />)
-                    }
-                    {showRisksLine && (<Line type="monotone" dataKey="riskPct" stroke="#B87273"
-                        strokeWidth={2}
-                        dot={{ fill: '#B87273', strokeWidth: 2, r: 4 }}
-                        activeDot={{ r: 6 }}
-                        name='With risk'
-                    />)}
-                    <RechartsDevtools />
-                </LineChart>
-            </div>
-        </div>
+                            name='With risk'
+                        />)}
+                        <RechartsDevtools />
+                    </LineChart>
+                </div>
+            </CardBody>
+        </Card>
     );
 
 }
