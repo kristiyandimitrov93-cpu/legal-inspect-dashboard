a430a7e0a2bacf34a599ca094d38efd1ce2c1b34
kristiyandimitrov93-cpu <kristiyan.dimitrov93@gmail.com>
2026-02-01 14:48:47 +0200

    using card for cases table

--- src/components/dashboard/cases/CasesTable.tsx
+++ src/components/dashboard/cases/CasesTable.tsx
@@ -2,6 +2,8 @@ import { MoreHorizontal } from 'lucide-react'
 import './CasesTable.scss'
 import type { RelevantCase } from '../../../types/relativeCasesApi'
 import { useGetRelevantCasesQuery } from '../../../api/relevantCasesApi'
+import { Card, CardBody, CardHeader, CardTitle } from '../../card/Card'
+import { CardMenuButton } from '../../common/MoreActionBtn'
 export const CasesTable = () => {
     const { data, error, isLoading } = useGetRelevantCasesQuery()
     debugger
@@ -10,50 +12,50 @@ export const CasesTable = () => {
     }
 
     return (
-        <div className="cases-table-card">
-            <div className="cases-table-card-header">
-                <h3> Relevant Cases</h3>
-                <button aria-label="More actions">
-                    <MoreHorizontal size={16} />
-                </button>
-            </div>
-            <div className='cases-table-container'>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Case Name</th>
-                            <th>Jurisdiction</th>
-                            <th>Year</th>
-                            <th>Relevance</th>
-                            <th>Clause Match</th>
-                            <th>Outcome</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data!.cases.map((caseItem: RelevantCase) => {
-                            return (<tr key={caseItem.id}>
-                                <td>
-                                    <span>{caseItem.caseName}</span>
-                                </td>
-                                <td>
-                                    <span>{caseItem.jurisdiction}</span>
-                                </td>
-                                <td>
-                                    <span>{caseItem.year}</span>
-                                </td>
-                                <td>
-                                    <span>{caseItem.relevance}</span>
-                                </td>
-                                <td>
-                                    <span>{caseItem.clauseMatch}</span>
-                                </td>
-                                <td><span>{caseItem.outcome}</span></td>
-                            </tr>)
-                        })}
-                    </tbody>
-                </table>
-            </div>
+        <Card className="cases-table-card">
+            <CardHeader actionBtn={<CardMenuButton />}>
+                <CardTitle> Relevant Cases</CardTitle>
 
-        </div>
+            </CardHeader>
+            <CardBody>
+                <div className='cases-table-container'>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Case Name</th>
+                                <th>Jurisdiction</th>
+                                <th>Year</th>
+                                <th>Relevance</th>
+                                <th>Clause Match</th>
+                                <th>Outcome</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data!.cases.map((caseItem: RelevantCase) => {
+                                return (<tr key={caseItem.id}>
+                                    <td>
+                                        <span>{caseItem.caseName}</span>
+                                    </td>
+                                    <td>
+                                        <span>{caseItem.jurisdiction}</span>
+                                    </td>
+                                    <td>
+                                        <span>{caseItem.year}</span>
+                                    </td>
+                                    <td>
+                                        <span>{caseItem.relevance}</span>
+                                    </td>
+                                    <td>
+                                        <span>{caseItem.clauseMatch}</span>
+                                    </td>
+                                    <td><span>{caseItem.outcome}</span></td>
+                                </tr>)
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            </CardBody>
+
+        </Card>
     )
 }
