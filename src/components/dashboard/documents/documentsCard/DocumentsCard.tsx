74e65d60abfa69aec883408d4e63e903fc7d4a7b
kristiyandimitrov93-cpu <kristiyan.dimitrov93@gmail.com>
2026-02-01 16:19:35 +0200

    adding documents card

---
+++ src/components/dashboard/documents/documentsCard/DocumentsCard.tsx
@@ -0,0 +1,41 @@
+import "./DocumentsCard.scss";
+import { ChevronDown } from "lucide-react";
+import { Card, CardHeader, CardTitle, CardSubtitle, CardBody, CardFooter } from "../../../card/Card";
+import { CardMenuButton } from "../../../common/MoreActionBtn";
+import { FileRow } from "../fileRow/FileRow";
+import { useSelector } from "react-redux";
+import { documentsSelector } from "../../../../state/documents";
+
+
+export interface DocumentCardProps {
+    onShowAll?: () => void
+}
+export const DocumentsCard = ({ onShowAll }: DocumentCardProps) => {
+    const documents = useSelector(documentsSelector)
+    return (
+        <Card className="documents-card">
+            <CardHeader
+                actionBtn={<CardMenuButton />}
+            >
+                <CardTitle>Documents</CardTitle>
+                <CardSubtitle>Last AI Documents Reviews</CardSubtitle>
+            </CardHeader>
+
+            <CardBody>
+                <div className="documents-list">
+                    {documents.map((doc) => (
+                        <div className="document-row" key={doc.name}>
+                            <FileRow documentFile={doc} />
+                        </div>
+                    ))}
+                </div>
+            </CardBody>
+
+            <CardFooter>
+                <button className="documents-show-all" onClick={onShowAll} type="button">
+                    Show all <ChevronDown size={16} />
+                </button>
+            </CardFooter>
+        </Card>
+    );
+}
